6088bd74d1b1756d2ca824220d93087bd97f4130
AutisticLulu <discord.meguminbot@gmail.com>
2025-07-08 12:27:55 +0200

    VS FNAF 3 StoryMenuState ported

--- source/backend/Difficulty.hx
+++ source/backend/Difficulty.hx
@@ -4,8 +4,7 @@ class Difficulty
 {
 	public static final defaultList:Array<String> = [
 		'Easy',
-		'Normal',
-		'Hard'
+		'Normal'
 	];
 	private static final defaultDifficulty:String = 'Normal'; //The chart that has no postfix and starting difficulty on Freeplay/Story Mode
 
